7709203c60e19beecbaf74c3563d9559cd4a8ceb
raselinfo <raselinfo52@gmail.com>
2023-07-29 00:01:13 +0600

    ✨ feat(kong.yml): add support for CORS plugin to allow cross-origin requests from https://mockbin.com with specific methods, headers, and credentials
    ✨ feat(index.js): add new route /remote to handle requests and return JSON response with name "this is remote server"
    
    📦 chore(package.json): add cors package as a dependency for handling CORS headers in the server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express=require("express")
+const cors=require("cors")
+
 const app=express()
 
 app.get("/echo",(req,res)=>{
     res.json({name:"rasel",age:25,sub:["bangla","english"]})
 })
+
+app.get("/remote",(req,res)=>{
+    res.json({name:"this is remote server"})
+})
+
 app.listen(4000,()=>{
     console.log("http://localhost:4000")
 })
