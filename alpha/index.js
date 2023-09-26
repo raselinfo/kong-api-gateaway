b3c83a7eea6c6dd0dea77f5ab74ef56987b194fd
rasel-stacklearner <rasel@stacklearner.com>
2023-09-26 16:21:43 +0600

    🔧 chore(alpha): comment out cache control headers in GET / and GET /echo routes for debugging purposes 🔧 chore(server): comment out cache control headers in GET /echo route for debugging purposes 🔧 chore(gama): comment out cache control headers in GET / and GET /echo routes for debugging purposes 🔧 chore(gama): comment out cache control headers in GET /echo route for debugging purposes 🔧 chore(gama): add localhost-key.pem and localhost.pem files for HTTPS support 🔧 chore(gama): update package.json to include mkcert dependency and start script

--- alpha/index.js
+++ alpha/index.js
@@ -4,7 +4,8 @@ const cors=require("cors")
 const app=express()
 
 app.get("/", (req, res) => {
-  res.setHeader("Cache-Control", "max-age=300");
+  // res.setHeader("Cache-Control", "max-age=300");
+  console.log(req.headers)
 
   res.json({
     message: "this is Alpha",
@@ -16,13 +17,15 @@ app.get("/", (req, res) => {
 
 
 app.get("/echo",(req,res)=>{
-    res.setHeader('Cache-Control', 'max-age=300'); 
+    // res.setHeader('Cache-Control', 'max-age=300'); 
+     console.log(req.headers)
 
     res.json({message:"this is Alpha",name:"rasel",age:25,sub:["bangla","english"]})
 })
 
 app.get("/remote",(req,res)=>{
-    res.setHeader('Cache-Control', 'max-age=300'); 
+    // res.setHeader('Cache-Control', 'max-age=300'); 
+     console.log(req.headers);
 
     res.json({message:"this is Alpha",name:"this is remote server"})
 })
--- gama/index.js
+++ gama/index.js
@@ -1,29 +1,36 @@
 const express=require("express")
 const cors=require("cors")
-
+const fs=require("fs")
+const https=require("https")
 const app=express()
 
+
+const key=fs.readFileSync("localhost-key.pem","utf-8")
+const cert=fs.readFileSync("localhost.pem","utf-8")
+
 app.get("/",(req,res)=>{
-    console.log("echo")
-    res.setHeader('Cache-Control', 'max-age=300'); 
+    console.log("echo",req.headers)
+    // res.setHeader('Cache-Control', 'max-age=300'); 
 
     res.json({message:"this is Gama server",name:"rasel",age:25,sub:["bangla","english"]})
 })
 
 app.get("/echo",(req,res)=>{
     console.log("echo")
-    res.setHeader('Cache-Control', 'max-age=300'); 
+    // res.setHeader('Cache-Control', 'max-age=300'); 
 
     res.json({message:"this is Gama server",name:"rasel",age:25,sub:["bangla","english"]})
 })
 
 app.get("/remote",(req,res)=>{
     console.log("remote")
-    res.setHeader('Cache-Control', 'max-age=300'); 
+    // res.setHeader('Cache-Control', 'max-age=300'); 
 
     res.json({message:"this is Gama server",name:"this is remote server"})
 })
 
-app.listen(4000,()=>{
-    console.log("http://localhost:4000")
+
+
+https.createServer({key,cert},app).listen(4000,()=>{
+    console.log("https://localhost:4000")
 })
