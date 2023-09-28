dc6cb2325178bcc989d343567e357af981628008
rasel-stacklearner <rasel@stacklearner.com>
2023-09-28 23:14:11 +0600

    📝 chore(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics
    📝 feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port
    
    📝 chore(pipeline.conf): add Logstash pipeline configuration file
    
    📝 chore(docker-compose.yml): update volume mapping for gama service to include node_modules directory
    
    📝 chore(docker-compose.yml): comment out zipkin service
    
    📝 chore(docker-compose.yml): add ELK stack services (Elasticsearch, Logstash, Kibana)
    
    📝 chore(gama/index.js): add axios dependency
    
    📝 feat(gama/index.js): add axios GET request to Kong service
    
    📝 chore(gama/package.json): add axios dependency

--- gama/index.js
+++ gama/index.js
@@ -3,7 +3,7 @@ const cors=require("cors")
 const fs=require("fs")
 const https=require("https")
 const app=express()
-
+const axios=require("axios")
 
 const key=fs.readFileSync("localhost-key.pem","utf-8")
 const cert=fs.readFileSync("localhost.pem","utf-8")
@@ -15,11 +15,16 @@ app.get("/",(req,res)=>{
     res.json({message:"this is Gama server",name:"rasel",age:25,sub:["bangla","english"]})
 })
 
-app.get("/echo",(req,res)=>{
-    console.log("echo")
+app.get("/echo",async(req,res)=>{
+   try{
+     console.log("echo")
     // res.setHeader('Cache-Control', 'max-age=300'); 
-
+    const response = await axios.get("http://kong:8000/alpha");
+    console.log("response >>>", response.data);
     res.json({message:"this is Gama server",name:"rasel",age:25,sub:["bangla","english"]})
+   }catch(err){
+    res.send(err)
+   }
 })
 
 app.get("/remote",(req,res)=>{
